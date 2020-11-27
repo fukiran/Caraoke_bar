5ef8b90fb1642e0e6b8412c84e657b30e4d337bd
fukiran <macdorphoto@yahoo.com>
2020-11-27 15:52:48 +0000

    guest tested

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,7 +1,14 @@
 import unittest
 
-#from src.quest import Guest
+from src.guest import Guest
 from src.song import Song
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest_1 = Guest("Pierre")
+        self.guest_2 = Guest("Alexander")
+        self.guest_3 = Guest("Pepe")
+
+
+    def test_guest_has_name(self):
+        self.assertEqual("Pierre",self.guest_1.name)
--- tests/song_test.py
+++ tests/song_test.py
@@ -6,13 +6,15 @@ class TestSong(unittest.TestCase):
     
     def setUp(self):
         self.song_1 = Song("Gotta Go", "Agnostic Front", 3.2)
+        self.song_2 = Song("On My Radio", "Selecter", 3.52)
+        self.song_3 = Song("Divorce a I'ltalienne", "Mungo's Hifi", 3.46)
 
 
-    def test_song_has_name(self):
+    def test_song_has_title(self):
         self.assertEqual("Gotta Go", self.song_1.title)
 
     def test_song_has_artist_name(self):
         self.assertEqual("Agnostic Front", self.song_1.artist)
 
     def test_song_has_duration(self):
-        self.assertEqual(True,isinstance(self.song_1.duration,float))
+        self.assertEqual(True,isinstance(self.song_2.duration,float))
